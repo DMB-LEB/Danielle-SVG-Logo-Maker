4efdaabb27c8196d774f2a544965d6e920cb0930
DMB-LEB <nhnative2020@gmail.com>
2023-09-04 16:31:15 -0400

    removed shape classes from index.js now that they are in lib/shapes.js

--- index.js
+++ index.js
@@ -2,33 +2,6 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const {Circle, Triangle, Square} = require('./lib/shapes');
 
-class Circle{
-    constructor(color){
-        this.color = color
-    }
-    render(){
-        return `<circle cx="150" cy="150" r="50" fill="${this.color}"/>`
-    }
-}
-
-class Square{
-    constructor(color){
-        this.color = color
-    }
-    render(){
-        return `<rect width="50" height="50" fill="${this.color}"/>`
-    }
-}
-
-class Triangle{
-    constructor(color){
-        this.color = color
-    }
-    render(){
-        return `<polygon points="50 15, 100 100, 0 100" fill="${this.color}"/>`
-    }
-}
-
 class SvgLogo{
     constructor(){
         this.textElement = ''
