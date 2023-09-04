9c8d5b635203c9b03165e4b9eaff13e12bd15f58
DMB-LEB <nhnative2020@gmail.com>
2023-09-04 16:13:38 -0400

    Added code to index.js

---
+++ index.js
@@ -0,0 +1,93 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+class Circle{
+    constructor(color){
+        this.color = color
+    }
+    render(){
+        return `<circle cx="150" cy="150" r="50" fill="${this.color}"/>`
+    }
+}
+
+class Square{
+    constructor(color){
+        this.color = color
+    }
+    render(){
+        return `<rect width="50" height="50" fill="${this.color}"/>`
+    }
+}
+
+class Triangle{
+    constructor(color){
+        this.color = color
+    }
+    render(){
+        return `<polygon points="50 15, 100 100, 0 100" fill="${this.color}"/>`
+    }
+}
+
+class SvgLogo{
+    constructor(){
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    render(){
+        return `<svg width="300" height="200"> ${this.shapeElement} ${this.textElement}</svg>`
+    }
+    setTextElement(text, color){
+        this.textElement = `<text x="150" y= "150" text-anchor="middle" fill="${color}"> ${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = shape.render();
+    }
+
+}
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'characters',
+      message: 'Please enter the characters you want to use for your logo, you may use up to three.',
+    },
+    {
+      type: 'input',
+      message: 'What color do you want the text to be?',
+      name: 'textColor',
+    },
+    {
+      type: 'list',
+      message: 'What shape do you want in your logo?',
+      name: 'shape',
+      choices: ['Circle', 'Triangle', 'Square'],
+    },
+    {
+      type: 'input',
+      message: 'What color do you want your shape to be?',
+      name: 'shapeColor',
+    },
+])
+.then((data) => {
+  const filename = `logo.svg`;
+  let createShape;
+
+    if (data.shape === 'Circle') {
+        createShape = new Circle(data.shapeColor);
+    } else if (data.shape === 'Triangle') {
+        createShape = new Triangle(data.shapeColor);
+    } else if (data.shape === 'Square') {
+        createShape = new Square(data.shapeColor);
+    }
+
+    const svg = new SvgLogo();
+    svg.setShapeElement(createShape);
+    svg.setTextElement(data.characters, data.textColor);
+    console.log(svg);
+
+  fs.writeFile(filename, svg.render(), (err) =>
+    err ? console.log(err) : console.log('Generated logo.svg')
+  );
+
+})
